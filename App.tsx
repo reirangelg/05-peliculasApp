3032f77bf8703f827aaa44dbd40c788bf0dc70b2
reirangelg <reirangelg@gmail.com>
2022-06-29 09:44:40 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import 'react-native-gesture-handler';
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigation } from './src/navigation/Navigation';
+import { FadeScreen } from './src/screens/FadeScreen';
+import { GradientProvider } from './src/context/GradientContext';
+
+const AppState = ({ children}: any ) => {
+
+  return (
+    <GradientProvider>
+      { children }
+    </GradientProvider>
+  )
+}
+
+const App = () => {
+
+  return (
+
+    <NavigationContainer>
+      <AppState>
+       <Navigation />
+     {/*} <FadeScreen /> */}
+     </AppState>
+    </NavigationContainer>
+  )
+}
+export default App
